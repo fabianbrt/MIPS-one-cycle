22f08b6d21a6c95c60af7cb0c722f86b5b15df83
fabianbrt <123643415+fabianbrt@users.noreply.github.com>
2025-03-10 10:45:02 +0200

    Update test_env.vhd ALU

--- test_env.vhd
+++ test_env.vhd
@@ -1,91 +1,98 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 02/24/2025 03:16:48 PM
--- Design Name: 
--- Module Name: test_env - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity test_env is
   Port (clk : in STD_LOGIC;
         btn : in STD_LOGIC_VECTOR (4 downto 0);
-        sw : in STD_LOGIC_VECTOR (15 downto 0);
-        led : out STD_LOGIC_VECTOR (15 downto 0);
+        sw : in STD_LOGIC_VECTOR (7 downto 0); 
         an : out STD_LOGIC_VECTOR (7 downto 0);
-        cat : out STD_LOGIC_VECTOR (6 downto 0));
+        cat : out STD_LOGIC_VECTOR (6 downto 0);
+        led : out STD_LOGIC_VECTOR (7 downto 0)); 
 end test_env;
 
 architecture Behavioral of test_env is
-signal CNT : std_logic_vector (7 downto 0) := (others => '0');
+
+
+signal CNT : std_logic_vector (1 downto 0) := "00";
 signal en : STD_LOGIC := '0';
-signal decoded: std_logic_vector(7 downto 0) := (others => '0');
+
+
+signal A, B : std_logic_vector(31 downto 0); 
+signal ALU_OUT : std_logic_vector(31 downto 0);
+signal ssd_digits: std_logic_vector(31 downto 0) := (others => '0'); 
+
+
+signal zero_flag : std_logic;
+
+
+signal ssd_an : std_logic_vector(7 downto 0);
+signal ssd_cat : std_logic_vector(6 downto 0);
+
+
 component MPG
     port (enable : out STD_LOGIC;
            btn : in STD_LOGIC;
-           clk : in STD_LOGIC
-    );
+           clk : in STD_LOGIC);
+end component;
+
+
+component SSD
+    Port ( digits : in STD_LOGIC_VECTOR (31 downto 0);
+           clk : in STD_LOGIC;
+           cat : out STD_LOGIC_VECTOR (6 downto 0);
+           an : out STD_LOGIC_VECTOR (7 downto 0));
 end component;
 
 begin
+
+
 e: MPG port map (
     enable => en,
     btn => btn(0),
     clk => clk
 );
-process(CLK)
+
+
+process(clk)
 begin
-if rising_edge(CLK) then
-    if en = '1' then
-        if sw(0) = '1' then
-            CNT <= CNT + 1;
-        else
-            CNT <= CNT - 1;
-        end if;    
+    if rising_edge(clk) then
+        CNT <= CNT + 1; 
     end if;
-end if;
 end process;
 
-process(cnt)
+
+A <= (others => '0') & sw(3 downto 0);  
+B <= (others => '0') & sw(7 downto 4);  
+
+
+process(CNT, A, B)
 begin
-case cnt is
- when "000" => decoded <= "00000000";
- when "001" => decoded <= "00000010";
- when "010" => decoded <= "00000100";
- when "011" => decoded <= "00001000";
- when "100" => decoded <= "00010000";
- when "101" => decoded <= "00100000";
- when "110" => decoded <= "01000000";
- when others => decoded <= "10000000";
-end case;
+    case CNT is
+        when "00" => ALU_OUT <= A + B;         
+        when "01" => ALU_OUT <= A - B;         
+        when "10" => ALU_OUT <= A sll 2;       
+        when others => ALU_OUT <= A srl 2;     
+    end case;
 end process;
 
 
+zero_flag <= '1' when ALU_OUT = X"00000000" else '0';
+led(7) <= zero_flag; 
+led(6 downto 0) <= (others => '0'); 
 
-led <= "00000000" & decoded;
+
+ssd_digits <= ALU_OUT;
+
+display: SSD port map (
+    digits => ssd_digits,
+    clk => clk,
+    cat => ssd_cat,
+    an => ssd_an
+);
+
+an <= ssd_an;
+cat <= ssd_cat;
+
 end Behavioral;
